ad7ef390e177c07149659b40bfb549ad196727e6
JoaoKashMeu <alecrim.joao@aluno.ifsp.edu.br>
2025-04-01 16:07:07 -0300

    exercício 2

---
+++ 02.py
@@ -0,0 +1,23 @@
+class Domino:
+    def __init__(self, face_a=0, face_b=0):
+        self.face_a = face_a
+        self.face_b = face_b
+
+    def mostrar_pontos(self):
+        return f"lado A: {self.face_a} lado B: {self.face_b}"
+
+    def valor(self):
+        return self.face_a + self.face_b
+
+    def __str__(self):
+        return f"Dominó ({self.face_a}, {self.face_b})"
+
+d1 = Domino(2, 6)
+d2 = Domino(4, 3)
+
+print(d1.mostrar_pontos())
+print(d2.mostrar_pontos())
+
+print("Total de pontos:", d1.valor() + d2.valor())
+
+print(d1)
